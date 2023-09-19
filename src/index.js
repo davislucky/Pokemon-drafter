9a5618a978e48a1107aeb69af3efc482245b628f
davislucky <58579323+davislucky@users.noreply.github.com>
2023-09-19 15:26:59 -0700

    Add more queries for items, abilities, and moves

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 
+const abilitiesDisplay = document.querySelector(".ability-selector-content");
+const typeDisplay = document.querySelector(".types-content");
+const baseStatsDisplay = document.querySelector(".base-stats-content");
 const pokedexList = document.getElementById("pokedex-list");
 const displayEntry = document.querySelector(".display");
 const preview = document.getElementById("preview-display");
+const movesDisplay = document.querySelector(".moves-selector-content");
+const movesList = document.querySelector(".move-selector-list");
+const itemsDisplay = document.querySelector(".item-selector-content");
+
 
 const fetchPokedex = async () => {
   const url = `https://pokeapi.co/api/v2/pokemon?limit=1010`; // want to eventually limit this query based on user's limits
@@ -38,19 +45,53 @@ const createButton = (entry) => {
   h2.innerHTML = entry.id + ". " + entry.name;
   button.appendChild(img);
   button.appendChild(h2);
-  button.addEventListener("click", handleClick);
+  button.addEventListener("click", handleClickPokedex);
   button.dataset.id = entry.id;
   button.dataset.name = entry.name;
   button.dataset.sprite = entry.sprite;
   return button;
 }
 
-const handleClick = async (event) => {
+const clearFields = () => {
+  clearChildren(movesDisplay);
+  clearChildren(baseStatsDisplay);
+  clearChildren(abilitiesDisplay);
+  clearChildren(typeDisplay);
+
+}
+
+const clearChildren = (element) => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild)
+  }
+}
+
+const handleClickPokedex = async (event) => {
+  clearFields();
   const target = event.currentTarget;
   changePreview(target.dataset.sprite);
   const pokemon = await fetchPokemonData(target.dataset.id);
   displayPokemon(pokemon);
 }
+const displayPokemon = (pokemon) => {
+  // console.log(pokemon);
+  displayMoves(pokemon.moves);
+  displayAbilities(pokemon.abilities);
+  displayType(pokemon.types);
+  displayEVs(pokemon.baseStatName, pokemon.evs);
+  displayIVs(pokemon.baseStatName, pokemon.ivs);
+  displayBaseStats(pokemon.baseStatName, pokemon.baseStat);
+  displayHeldItems();
+}
+
+const displayType = (types) => {
+  types.forEach(type => {
+    const display = document.createElement("p");
+    display.innerText = `${type}`;
+    typeDisplay.appendChild(display);
+  });
+}
+
 
 const changePreview = (src) => {
   preview.src = src;
@@ -67,16 +108,147 @@ const fetchPokemonData = async (id) => {
   pokemon.baseStat = data.stats.map((baseStat) => baseStat.base_stat);
   pokemon.baseStatName = data.stats.map((name) => name.stat.name);
   pokemon.evs = data.stats.map((ev) => ev.effort);
+  pokemon.ivs = data.stats.map((ev) => ev.effort);
   pokemon.height = data.height; // measured in meters so divide by 10
   pokemon.weight = data.weight; //measured in kgs so divide by 10
   return pokemon;
 }
 
-const displayPokemon = (pokemon) => {
-  console.log(pokemon);
+
+
+const fetchMoveInfo = async (move) => {
+  const url = `https://pokeapi.co/api/v2/move/${move}`
+  const res = await fetch(url);
+  const data = await res.json();
+  const moveInfo = {};
+  
+  moveInfo.name = data.name;
+  moveInfo.type = data.type.name;
+  moveInfo.power = data.power;
+  moveInfo.accuracy = data.accuracy;
+  moveInfo.class = data.damage_class.name;
+  moveInfo.url = `https://pokemondb.net/move/${move}`
+  return moveInfo;
 }
+
+const displayMoves = (moves) => {
+  moves.forEach(move => {
+    displayMove(move);
+  });
+}
+
+const displayMove = async (move) => {
+  const moveInfo = await fetchMoveInfo(move);
+  const display = document.createElement("li");
+  // display.setAttribute("value", `${move}`);
+  display.innerText = `${moveInfo.type}`.split("-").join(" ")
+  movesList.appendChild(display);
+  movesDisplay.appendChild(movesList);
+}
+
+
+
+const fetchHeldItems = async () => {
+  const url = `https://pokeapi.co/api/v2/item-attribute/holdable`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const items = data.items;
+  return items;
+}
+
+const fetchHeldItemInfo = async (item) => {
+  const url = `https://pokeapi.co/api/v2/item/${item}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  const itemInfo = {};
+
+  itemInfo.name = data.name;
+  itemInfo.effect = data.effect_entries;
+  return itemInfo;
+}
+
+const displayHeldItems = async () => {
+  const items = await fetchHeldItems();
+  items.forEach(item => {
+    const listEl = document.createElement("li")
+    const a = document.createElement("a");
+    const name = item.name;
+    console.log(name)
+    a.setAttribute("href", `https://pokemondb.net/item/${name}`);
+    a.innerText = `${item.name} `;
+    listEl.appendChild(a);
+    itemsDisplay.appendChild(listEl);
+    console.log(fetchHeldItemInfo(item.name))
+  })
+}
+
+
+const displayAbilities = (abilities) => {
+  abilities.forEach(ability => {
+    const a = document.createElement("a");
+    a.setAttribute("href", `https://pokemondb.net/ability/${ability}`);
+    a.innerText = `${ability} `;
+    abilitiesDisplay.appendChild(a);
+  });
+
+  console.log(abilities)
+}
+
+const displayBaseStats = (names, baseStats) => {
+  const stats = {};
+
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+
+    for (let j = 0; j < baseStats.length; j++) {
+      const number = baseStats[j];
+
+      stats[name] = number;
+    }
+  }
+  console.log(stats);
+  
+
+  // Object.entries(stats).forEach(stat => {
+  //   const display = document.createElement("p");
+  //   display.innerText = `${stat}`
+  //   baseStatsDisplay.appendChild(display); //not working
+  // });
+
+}
+
+const displayEVs = (names, evs) => {
+  const stats = {};
+
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+
+    for (let j = 0; j < evs.length; j++) {
+      stats[name] = 0;
+    }
+  }
+  console.log(stats);
+  return stats;
+}
+
+const displayIVs = (names, ivs) => {
+  const stats = {};
+
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+
+    for (let j = 0; j < ivs.length; j++) {
+      stats[name] = 0;
+    }
+  }
+  console.log(stats);
+  return stats;
+}
+
 const main  = async () => {
     const pokedex = await fetchPokedex();
     displayPokedex(pokedex);
 }
 main();
+
